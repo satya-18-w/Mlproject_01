3816122a371ec2b1982a91b74d7bf0c1d34d55ff
satya-18-w <satyajitsamal198076@gmail.com>
2024-08-21 10:17:52 +0530

    After adding e

--- setup.py
+++ setup.py
@@ -2,34 +2,22 @@ from setuptools import find_packages,setup
 from typing import List
 
 
-
-HYPEN_E_DOT="-e ."
-
-def get_requirements(file_path:str)->List[str]:
-    '''
-    This function will return the requirements
-    '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-            
-    return requirements
-        
+def get_requirements()->list[str]:#return list of string
+    requirements_list=list[str]=[]
     
+    return requirements_list
+
+
 
 
 setup(
-    
-    name="Ml_project",
+    name="sensor",
     version="0.0.1",
-    author="Satyajit samal",
+    author="Satyajit",
     author_email="satyajitsamal198076@gmail.com",
     packages=find_packages(),
-    install_requires=get_requirements("requirements.txt")
+    install_requires=get_requirements()
+    
     
     
     
